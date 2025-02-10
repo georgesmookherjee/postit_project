dcbc4993307e2810dc87346013f0490805163215
georges mookherjee <georges.mookherjee@lilo.org>
2025-02-10 17:37:33 +0100

    2/3 modifs vite fait

--- app/routes.py
+++ app/routes.py
@@ -21,20 +21,6 @@ def ping_db():
     except Exception as erreur:
         return jsonify({'error': str(erreur)}), 500
 
-# @routes_app.route('/modifier/<int:postit_id>', methods=['GET', 'POST'])
-# def modifier_postit(postit_id):
-#     postit = PostIt.query.get(postit_id)
-    
-#     if request.method == 'POST':
-#         postit.titre = request.form['titre']
-#         postit.contenu = request.form['contenu']
-#         db.session.commit()
-#         flash("Post-it modifié avec succès", "success")
-#         return redirect(url_for('html.afficher_postits'))
-
-#     return render_template('modifier_postit.html', postit=postit)
-
-
 @routes_app.route('/api/postits/<int:postit_id>', methods=['DELETE'])
 def supprimer_postit(postit_id):
     postit = db.session.get(PostIt, postit_id)
@@ -59,3 +45,16 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(routes_app.root_path, 'static'), 'favicon.ico')
 
+
+# @routes_app.route('/modifier/<int:postit_id>', methods=['GET', 'POST'])
+# def modifier_postit(postit_id):
+#     postit = PostIt.query.get(postit_id)
+    
+#     if request.method == 'POST':
+#         postit.titre = request.form['titre']
+#         postit.contenu = request.form['contenu']
+#         db.session.commit()
+#         flash("Post-it modifié avec succès", "success")
+#         return redirect(url_for('html.afficher_postits'))
+
+#     return render_template('modifier_postit.html', postit=postit)
