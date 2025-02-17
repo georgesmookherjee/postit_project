c44cc3aaeaad659c5d550bf6cd51a65e0e4a289a
georges mookherjee <georges.mookherjee@lilo.org>
2025-02-17 12:28:18 +0100

    Correction du démarrage Flask

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ app.config.from_object(current_config)
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
-    app.run(debug=(current_config == "development"))
+    app.run(host="0.0.0.0", port=5000, debug=False)
