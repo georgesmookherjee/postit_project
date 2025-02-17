4cd763228462f1b26b9a28eaac5fd87fe355a5e1
georges mookherjee <georges.mookherjee@lilo.org>
2025-02-17 13:36:51 +0100

    Correction du chemin des templates Flask

--- run.py
+++ run.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from config import current_config
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-# Importation des Blueprints
-from app.routes import routes_app
-from app.api.postits_api import api_bp  # Assure-toi que c'est bien le bon chemin
+# Spécifier le dossier templates explicitement
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), "app", "templates"))
 
-app = Flask(__name__)
 app.config.from_object(current_config)
 
 db = SQLAlchemy(app)
 
-# Enregistrement des Blueprints
+# Importer et enregistrer les Blueprints
+from app.routes import routes_app
+from app.api.postits_api import api_app  
+
 app.register_blueprint(routes_app)
-app.register_blueprint(api_bp, url_prefix="/api")  # Si tes routes API sont sous /api
+app.register_blueprint(api_app, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
